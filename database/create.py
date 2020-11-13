29300b5b1a973fcf29825d2de8b439ef6c599f2c
Maryline <marybot@free.fr>
2020-11-13 16:46:34 +0100

    Update venv

--- database/create.py
+++ database/create.py
@@ -1,3 +1,18 @@
 """ Create database """
+
 import mysql.connector
 
+class database :
+
+    mydb=mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Ma25Bo77Yi181"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("SHOW DATABASES")
+
+    for x in mycursor:
+        print(x)

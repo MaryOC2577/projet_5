231f2071c9ae19fc27b6faf0f40b9921fcdf8457
Maryline <marybot@free.fr>
2020-11-18 19:42:56 +0100

    mysql connector

--- database/create.py
+++ database/create.py
@@ -6,4 +6,9 @@ mydb = mysql.connector.connect(
     host="localhost", user="root", password="Ma25Bo77Yi181"
 )
 
-print(mydb)
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+    print(x)

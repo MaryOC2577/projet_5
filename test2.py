bba9b01fdad0654de9a7428ed734c77b184da59a
Maryline <marybot@free.fr>
2020-12-10 20:46:26 +0100

    Update test2

--- test2.py
+++ test2.py
@@ -2,6 +2,21 @@
 
 import requests
 
-res = requests.get("https://scotch.io")
+payload = {
+    "search_terms": "",
+    # "search_tag": "brands",
+    "sort_by": "unique_scans_n",
+    "page_size": 100,
+    "json": 1,
+}
 
-print(res)
+res = requests.get(
+    "https://fr.openfoodfacts.org/cgi/search.pl?", params=payload
+)
+
+result = res.json()
+products = result["products"]
+
+
+for product in products:
+    print(product["product_name"])

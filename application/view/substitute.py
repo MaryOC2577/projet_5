55c2e550eb69d1358b6ca94785f8df874033a07e
Maryline <marybot@free.fr>
2021-01-31 14:00:57 +0100

    Create substitute view

---
+++ application/view/substitute.py
@@ -0,0 +1,12 @@
+"""Substitute display menu."""
+
+
+class SubstituteMenu:
+    """Substitute display menu."""
+
+    def __init__(self):
+        """Initialize the substitute display menu."""
+
+    def show(self):
+        """Show a substitute."""
+        print("Substitut du produit sélectionné : ")

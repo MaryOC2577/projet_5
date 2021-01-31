66eabe6bd0262ff16708c6f109c0a86818f5616c
Maryline <marybot@free.fr>
2021-01-31 13:53:33 +0100

    Create product view

--- application/view/product.py
+++ application/view/product.py
@@ -1,1 +1,12 @@
 """Product selection menu."""
+
+
+class ProductMenu:
+    """Product selection menu."""
+
+    def __init__(self):
+        """Initialize product selection menu."""
+
+    def show(self):
+        """Show the product selection menu."""
+        print("Sélectionnez l'aliment : ")

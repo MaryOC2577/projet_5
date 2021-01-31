c6e674058aff189e83b9890dc3a1843fb549fe01
Maryline <marybot@free.fr>
2021-01-31 20:57:10 +0100

    Create product menu controller

---
+++ application/controller/productmenu.py
@@ -0,0 +1,15 @@
+"""Controller of the product menu."""
+
+from application.view.product import ProductMenu
+
+
+class ProductMenuController:
+    """Controller of the product menu."""
+
+    def __init__(self):
+        """Initializa the controller of the product menu."""
+
+    def show(self):
+        """Show the controller of the product menu."""
+        product_menu = ProductMenu()
+        product_menu.show()
